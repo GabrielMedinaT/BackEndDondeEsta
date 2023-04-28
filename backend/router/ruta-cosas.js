2704488fa72baba51baf7a7f678a472301a0bc5c
Gabriel <entropia7@gmail.com>
2023-04-28 09:28:12 +0100

    v1

--- backend/router/ruta-cosas.js
+++ backend/router/ruta-cosas.js
@@ -21,107 +21,9 @@ router.get("/", async (req, res) => {
 
 //*CREAR COSAS
 
-router.post("/nueva", async (req, res, next) => {
+router.post("/nuevo", async (req, res, next) => {
+  const { nombre, cajon, armario, habitacion, casa } = req.body;
   let existeCajon, existeArmario, existeHabitacion, existeCasa;
-  const {
-    nombre,
-    descripcion,
-    clasificacion,
-    cajon,
-    armario,
-    habitacion,
-    casa,
-    caja,
-    prestado,
-  } = req.body;
-  if (casa && existeCasa) {
-    return res.json({ message: "La cosa ya está asignada a otra casa" });
-  }
-
-  if (cajon) {
-    existeCajon = await Cajon.findOne({ nombre: cajon });
-    if (!existeCajon) {
-      return res.json({
-        message: `${nombre} debe estar guardado en un cajón existente`,
-      });
-    }
-    existeArmario = await Armario.findById(existeCajon.armario);
-    existeHabitacion = await Habitacion.findById(existeArmario.habitacion);
-    existeCasa = await Casa.findById(existeHabitacion.casa);
-  } else if (armario) {
-    existeArmario = await Armario.findOne({ nombre: armario });
-    existeHabitacion = await Habitacion.findById(existeArmario.habitacion);
-    existeCasa = await Casa.findById(existeHabitacion.casa);
-  } else if (habitacion) {
-    existeHabitacion = await Habitacion.findOne({ nombre: habitacion });
-    existeCasa = await Casa.findById(existeHabitacion.casa);
-  } else if (casa) {
-    existeCasa = await Casa.findOne({ nombre: casa });
-  } else {
-    return res.json({
-      message:
-        "Se debe especificar al menos una propiedad de ubicación (cajón, armario, habitación, casa)",
-    });
-  }
-
-  const cosa = new Cosa({
-    nombre,
-    descripcion,
-    clasificacion,
-    cajon: existeCajon ? existeCajon._id : undefined,
-    armario: existeArmario ? existeArmario._id : undefined,
-    habitacion: existeHabitacion ? existeHabitacion._id : undefined,
-    casa: existeCasa ? existeCasa._id : undefined,
-    prestado,
-    caja,
-  });
-  try {
-    const cosaGuardada = await cosa.save();
-
-    if (existeCasa) {
-      await Casa.findByIdAndUpdate(existeCasa._id, {
-        $push: { cosas: cosaGuardada._id },
-      });
-    } else {
-      return res.json({
-        message: `${nombre} debe estar guardado al menos en una casa o el nombre de la casa está mal escrito`,
-      });
-    }
-    if (existeCajon) {
-      await Cajon.findByIdAndUpdate(existeCajon._id, {
-        $push: { cosas: cosaGuardada._id },
-      });
-    }
-    if (existeArmario) {
-      await Armario.findByIdAndUpdate(existeArmario._id, {
-        $push: { cosas: cosaGuardada._id },
-      });
-    }
-    if (existeHabitacion) {
-      await Habitacion.findByIdAndUpdate(existeHabitacion._id, {
-        $push: { cosas: cosaGuardada._id },
-      });
-    }
-    res.json(cosaGuardada);
-  } catch (err) {
-    res.json({ message: err });
-  }
-});
-
-//*MODIFICAR COSA
-
-router.patch("/editar/:nombre", async (req, res, next) => {
-  const { nombre } = req.params;
-  const { cajon, armario, habitacion, casa } = req.body;
-
-  let existeCosa = await Cosa.findOne({ nombre: nombre });
-  if (!existeCosa) {
-    res.json({ message: "La cosa no existe" });
-    return;
-  }
-
-  let existeCajon, existeArmario, existeHabitacion, existeCasa;
-
   if (cajon) {
     existeCajon = await Cajon.findOne({ nombre: cajon });
     if (!existeCajon) {
@@ -153,7 +55,63 @@ router.patch("/editar/:nombre", async (req, res, next) => {
       return;
     }
   }
+  const cosa = new Cosa({
+    nombre,
+    cajon: existeCajon ? existeCajon._id : null,
+    armario: existeArmario ? existeArmario._id : null,
+    habitacion: existeHabitacion ? existeHabitacion._id : null,
+    casa: existeCasa ? existeCasa._id : null,
+  });
+  try {
+    const cosaGuardada = await cosa.save();
+    res.json(cosaGuardada);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
+//*MODIFICAR COSA
+
+router.patch("/editar/:nombre", async (req, res, next) => {
+  const { nombre } = req.params;
+  const { cajon, armario, habitacion, casa } = req.body;
+  let existeCosa = await Cosa.findOne({ nombre: nombre });
+  if (!existeCosa) {
+    res.json({ message: "La cosa no existe" });
+    return;
+  }
+  let existeCajon, existeArmario, existeHabitacion, existeCasa;
+  if (cajon) {
+    existeCajon = await Cajon.findOne({ nombre: cajon });
+    if (!existeCajon) {
+      res.json({ message: "El cajón no existe" });
+      return;
+    }
+    existeArmario = await Armario.findById(existeCajon.armario);
+    existeHabitacion = await Habitacion.findById(existeArmario.habitacion);
+    existeCasa = await Casa.findById(existeHabitacion.casa);
+  } else if (armario) {
+    existeArmario = await Armario.findOne({ nombre: armario });
+    if (!existeArmario) {
+      res.json({ message: "El armario no existe" });
+      return;
+    }
+    existeHabitacion = await Habitacion.findById(existeArmario.habitacion);
+    existeCasa = await Casa.findById(existeHabitacion.casa);
+  } else if (habitacion) {
+    existeHabitacion = await Habitacion.findOne({ nombre: habitacion });
+    if (!existeHabitacion) {
+      res.json({ message: "La habitación no existe" });
+      return;
+    }
+    existeCasa = await Casa.findById(existeHabitacion.casa);
+  } else if (casa) {
+    existeCasa = await Casa.findOne({ nombre: casa });
+    if (!existeCasa) {
+      res.json({ message: "La casa no existe" });
+      return;
+    }
+  }
   await Cajon.findByIdAndUpdate(existeCosa.cajon, {
     $pull: { cosas: existeCosa._id, new: true },
   });
@@ -190,7 +148,6 @@ router.patch("/editar/:nombre", async (req, res, next) => {
       $pull: { cosas: existeCosa._id },
     });
   }
-
   if (existeHabitacion) {
     await Habitacion.findByIdAndUpdate(existeHabitacion._id, {
       $push: { cosas: existeCosa._id },
@@ -203,7 +160,6 @@ router.patch("/editar/:nombre", async (req, res, next) => {
       $pull: { cosas: existeCosa._id },
     });
   }
-
   await Casa.findByIdAndUpdate(existeCasa._id, {
     $push: { cosas: existeCosa._id },
   });
