5c7d45fa0628bcd68943763dbbcdfddad1e655a0
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-02 21:39:42 +0100

    v1

--- backend/router/ruta-casa.js
+++ backend/router/ruta-casa.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 const autorizacion = require("../middleware/checkAuth");
 
 // router.use(cors());
-
+router.use(cors());
 router.use(checkAuth);
 //*VER CASAS
 
@@ -100,67 +100,59 @@ router.patch("/editar/:nombre", async (req, res, next) => {
 
 // Ruta para borrar una casa
 router.delete("/borrar/:id", autorizacion, async (req, res, next) => {
-  const session = await mongoose.startSession();
   try {
-    await session.withTransaction(async () => {
-      const casaBuscar = await Casa.findById(req.params.id).session(session);
+    const casaBuscar = await Casa.findById(req.params.id);
 
-      if (!casaBuscar) {
-        res.json({ message: "No existe la casa" });
-        return next();
-      }
+    if (!casaBuscar) {
+      res.json({ message: "No existe la casa" });
+      return next();
+    }
 
-      // Validar que el usuario autenticado sea el mismo que creó la casa
-      if (casaBuscar.usuario.toString() !== req.datosUsuario.userId) {
-        res.status(404).json({ message: "La casa no existe" });
-        return next();
-      }
+    // Validar que el usuario autenticado sea el mismo que creó la casa
+    if (casaBuscar.usuario.toString() !== req.datosUsuario.userId) {
+      res.status(404).json({ message: "La casa no existe" });
+      return next();
+    }
 
-      // Obtener las cosas de la casa que se va a eliminar
-      const cosas = casaBuscar.cosas;
+    // Obtener las cosas de la casa que se va a eliminar
+    const cosas = casaBuscar.cosas;
 
-      // Borrar todas las habitaciones de la casa y los armarios y cajones asociados
-      await Habitacion.deleteMany({ casa: casaBuscar._id }).session(session);
-      const armariosBuscar = await Armario.find({
-        habitacion: { $in: casaBuscar.habitaciones },
-      }).session(session);
-      const armariosIds = armariosBuscar.map((armario) => armario._id);
-      await Cajon.deleteMany({ armario: { $in: armariosIds } }).session(
-        session
-      );
-      await Armario.deleteMany({
-        habitacion: { $in: casaBuscar.habitaciones },
-      }).session(session);
-
-      // Crear una nueva instancia de Caja y asignarle las cosas de la casa
-      const nuevaCaja = new Caja({
-        nombre: `${casaBuscar.nombre}_caja`,
-        cosas: cosas,
-      });
-
-      // Guardar la nueva caja en la base de datos
-      const cajaGuardada = await nuevaCaja.save();
-
-      await Casa.findByIdAndDelete(req.params.id).session(session);
-
-      let usuario = await Usuario.findById({ casas: casaBuscar._id }).session(
-        session
-      );
-
-      if (!usuario) {
-        res.json({ message: "El usuario no existe" });
-        return next();
-      }
-
-      usuario.casas.pull(casaBuscar._id);
-
-      res.json({ message: "Casa borrada y cosas asignadas a la nueva caja" });
+    // Borrar todas las habitaciones de la casa y los armarios y cajones asociados
+    await Habitacion.deleteMany({ casa: casaBuscar._id });
+    const armariosBuscar = await Armario.find({
+      habitacion: { $in: casaBuscar.habitaciones },
     });
+    const armariosIds = armariosBuscar.map((armario) => armario._id);
+    await Cajon.deleteMany({ armario: { $in: armariosIds } });
+    await Armario.deleteMany({
+      habitacion: { $in: casaBuscar.habitaciones },
+    });
+
+    // Crear una nueva instancia de Caja y asignarle las cosas de la casa
+    const nuevaCaja = new Caja({
+      nombre: `${casaBuscar.nombre}_caja`,
+      cosas: cosas,
+    });
+
+    // Guardar la nueva caja en la base de datos
+    const cajaGuardada = await nuevaCaja.save();
+
+    await Casa.deleteOne({ _id: req.params.id });
+    Casa.save();
+
+    let usuario = await Usuario.findById({ casas: casaBuscar._id });
+
+    if (!usuario) {
+      res.json({ message: "El usuario no existe" });
+      return next();
+    }
+
+    usuario.casas.pull(casaBuscar._id);
+
+    res.json({ message: "Casa borrada y cosas asignadas a la nueva caja" });
   } catch (err) {
     res.json({ message: "No se pudo borrar la casa" });
     return next(err);
-  } finally {
-    session.endSession();
   }
 });
 
