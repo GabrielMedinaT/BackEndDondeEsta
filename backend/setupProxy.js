acfd862f6e1f11047d96a4a4cf49cc4c62006964
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-05-14 21:07:59 +0100

    auth

--- backend/setupProxy.js
+++ backend/setupProxy.js
@@ -8,4 +8,32 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
+  app.use(
+    "/auth",
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    "/api",
+    "/auth",
+    "/login",
+    "/register",
+    "/logout",
+    "/api/casa",
+    "/api/habitaciones",
+    "/api/armario",
+    "/api/cajon",
+    "/api/cosa",
+    "/api/usuario/login",
+    "/api/usuario/registro",
+    "/api/usuarios/password",
+    "/api/casas/nueva",
+    "/api/habitaciones/nueva",
+    createProxyMiddleware({
+      target: "https://whereisthe.netlify.app/",
+      changeOrigin: true,
+    })
+  );
 };
